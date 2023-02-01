78633ddcbb6b87fbec465a653b4d2a3d7bf7552c
tahir <tahir123454@gmail.com>
2023-02-01 18:05:14 +0500

    start lab 1

--- labs/lab_1.cpp
+++ labs/lab_1.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 #include <span>
 #include <vector>
-
+#include <functional>
+#include <fstream>
+#include <tuple>
 #include "matrix_solvers.hpp"
 
-int main(int, char **)
-{
-	double A[4] = {0, 1, 1, 1};
-	double B[4] = {2, 10, -5, 4};
-	double C[4] = {1, -5, 2, 0};
-	double D[4] = {-5, -18, -40, -27};
-	auto X = solve_tridiagonal(A, B, C, D);
+const double EPS = 10e-6;
 
-	double target_x[] = {-3, 1, 5, -8};
-	for (int i = 0; i < 4; ++i)
-	{
-		std::cout << X[i] << " ";
+auto euler_method(std::function<double(double, double)> foo, double y, double t, double h, int n) {
+	std::vector<std::pair<double, double>> result;
+    for (int i = 0; i < n; i++) {
+        y += h * foo(y, t);
+        t += h;
+
+		result.push_back(std::pair(t, y)); // handler
+    }
+
+	return result;
+}
+
+int main() {
+	auto foo = [](double y, double t) { return 6*t*t + 5*t*y; };
+	auto res = euler_method(foo, 1, 1, 0.01, 10);
+
+	std::fstream output;
+
+	output.open("../../../results/lab_1/ex1.txt", std::ios::out);
+	if (!output) {
+		std::cout << "File not created!";
 	}
-
+	else {
+		for (auto [t, y] : res) {
+			output << t << "\t" << y << "\n";
+		}
+		output.close();
+	}
     return 0;
 }
